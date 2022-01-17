8adbe902009c1f0d93c773e4c0be134ae3996462
Rob Curtis <Robanthonycurtis@gmail.com>
2022-01-17 13:27:26 -0800

    element from array using index

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -1,9 +1,9 @@
 var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
 console.log('languages array', languages);
 
-var firstElement = 'html';
+var firstElement = languages[0];
 console.log('the first element of the array is', firstElement);
-var thirdElement = 'javascript';
+var thirdElement = languages[2];
 console.log('the 3rd element of the array is', thirdElement);
 const length = languages.length;
 console.log('the length of the array is', length);
