df8f2eab880f9d1c166d9bc0969f2fd36883a430
Rob Curtis <Robanthonycurtis@gmail.com>
2022-01-17 12:48:03 -0800

    deleted prep-css-layout-classes/index.html

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,23 @@
+var person = {
+  firstName: 'Robert',
+  lastName: 'Curtis',
+  hobby: 'Surfing'
+};
+
+const firstName = 'Robert ';
+const lastName = 'Curtis ';
+const hobby = 'Surfing';
+console.log(person);
+
+const fullName = firstName + lastName;
+console.log(fullName);
+
+person.job = 'Uber Eats Driver';
+const job = 'Uber Eats Driver';
+console.log('This persons job is:', job);
+person.previousjob = 'Franciscan Friar';
+const previousjob = 'Franciscan Friar';
+console.log('This persons previous job was:', previousjob);
+
+console.log(hobby);
+console.log(person);
